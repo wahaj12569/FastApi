f5460d137a703bd7792eac062f0d5db70d6ca199
wahaj12569 <wahaj12569@gmail.com>
2024-11-21 15:48:07 +0500

    first

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+@app.get('/')
+def index():
+    return {'data':{'name':'wahaj'}}
+
+
+@app.get('/about')
+
+def about():
+    return {'data':'this is about page'}
