2fd453571e70978b95880a26eca3acaf23f763be
wahaj12569 <wahaj12569@gmail.com>
2024-11-22 14:33:58 +0500

    11/22/24

--- main.py
+++ main.py
@@ -7,6 +7,21 @@ def index():
 
 
 @app.get('/about')
-
 def about():
     return {'data':'this is about page'}
+
+@app.get('/blog')
+def index(limit,published:bool):
+    return published
+    if published:
+        return {'data':f'{limit} published blogs from the DB'}
+    else:
+        return {'data':f'{limit} blogs from the DB'}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data':'unpublished'}
+
+@app.get('/blog/{id}') #It is dynamic routing and in dynamic routing the block of dynamic routing should be blow the others block!! 
+def show(id : int):
+    return {'data':id}
